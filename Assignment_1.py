ab9effb45b6f3ac2a6dcd298e92f19e940492032
Oisin <oisin.mangan@gmail.com>
2015-10-09 12:26:15 +0100

    Code added to calculate distances between locations and distances between locations and ports

--- Assignment_1.py
+++ Assignment_1.py
@@ -8,18 +8,7 @@ Created on Mon Oct 05 22:23:29 2015
 import math
 import sqlite3
 
-conn = sqlite3.connect('renewable.db') # create a "connection"
-c = conn.cursor() # create a "cursor" 
-c.execute("SELECT * FROM ports;") # execute a SQL command
-for item in c:
-    print item
-
-c.execute("SELECT * FROM location;") # execute a SQL command
-for item in c:
-    print item[2]
-    print (type(item))
- 
-def calculate_distance(lat1, long1, lat2, long2):
+def lat_long_distance(long1, lat1, long2, lat2):
     # Convert latitude and longitude to spherical coordinates in radians.
     degrees_to_radians = math.pi/180.0     
     # phi = 90 - latitude
@@ -38,5 +27,46 @@ def calculate_distance(lat1, long1, lat2, long2):
     distance = 6373 * arc
     return distance
 
-distance = calculate_distance(6.25, 53.33, 6.39, 52.27)
-print distance
+
+conn = sqlite3.connect('renewable.db') # create a "connection"
+c = conn.cursor() # create a "cursor" 
+c.execute("SELECT * FROM ports;") # execute a SQL command
+ports = []
+for item in c:
+    ports.append(item)
+
+
+c.execute("SELECT * FROM location;") # execute a SQL command
+location = []
+for item in c:
+    location.append(item)
+print location
+    
+def distance_between_plants(plant_num):
+    proposed_plant_data = location[plant_num]
+    print proposed_plant_data
+    distance_between_plants = []
+    for alternative_plant_data in location:
+        if proposed_plant_data != alternative_plant_data:
+            test = lat_long_distance(alternative_plant_data[0], alternative_plant_data[1], proposed_plant_data[0], proposed_plant_data[1])
+            distance_between_plants.append(test)
+    print distance_between_plants
+    
+#print(len(location))
+for i in range(len(location)):
+    print i
+    distance_between_plants(i)
+
+def distance_plants_to_ports(plant_num):
+    proposed_plant_data = location[plant_num]
+    print proposed_plant_data
+    distance_plants_to_ports = []
+    for port_data in ports:
+        test = lat_long_distance(port_data[0], port_data[1], proposed_plant_data[0], proposed_plant_data[1])
+        distance_plants_to_ports.append(test)
+    print distance_plants_to_ports
+    
+for i in range(len(location)):
+    print i
+    distance_plants_to_ports(i)
+  
